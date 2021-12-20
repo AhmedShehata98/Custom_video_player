f858263cf8c64254bd4fd6883cb64f47306ab5e9
AhmedShehata98 <userstore98@gmail.com>
2021-12-20 23:27:22 +0200

    #002 : fixed Err 404

--- js/videos.js
+++ js/videos.js
@@ -2,47 +2,47 @@ var videosList =
 [
     {
         videoName   : 'The Amazing Sunset in the sea' ,
-        videoPath   : '../videos/videos-list/video00.mp4' ,
+        videoPath   : 'videos/videos-list/video00.mp4' ,
         channelName : 'Pexels' ,
-        channelLogo : '../img/img-list/pexels-logo.svg'
+        channelLogo : 'img/img-list/pexels-logo.svg'
     }
     ,
     {
         videoName   : 'Story of Creator Editing Time' ,
-        videoPath   : '../videos/videos-list/video01.mp4' ,
+        videoPath   : 'videos/videos-list/video01.mp4' ,
         channelName : 'Pexels' ,
-        channelLogo : '../img/img-list/pexels-logo.svg'
+        channelLogo : 'img/img-list/pexels-logo.svg'
     }
     ,
     {
         videoName   : 'Digital Formation of DNA In an animated Presntitions' ,
         channelName : 'Pexels' ,
-        videoPath   : '../videos/videos-list/video02.mp4' ,
-        channelLogo : '../img/img-list/pexels-logo.svg',
+        videoPath   : 'videos/videos-list/video02.mp4' ,
+        channelLogo : 'img/img-list/pexels-logo.svg',
 
     }
     ,
     {
         videoName   : 'A Sparkle of liquid in a black background' ,
-        videoPath   : '../videos/videos-list/video03.mp4' ,
+        videoPath   : 'videos/videos-list/video03.mp4' ,
         channelName : 'Pexels' ,
-        channelLogo : '../img/img-list/pexels-logo.svg'
+        channelLogo : 'img/img-list/pexels-logo.svg'
 
     }
     ,
     {
         videoName   : 'Water crashing over the Rocks' ,
-        videoPath   : '../videos/videos-list/video04.mp4' ,
+        videoPath   : 'videos/videos-list/video04.mp4' ,
         channelName : 'Pexels' ,
-        channelLogo : '../img/img-list/pexels-logo.svg'
+        channelLogo : 'img/img-list/pexels-logo.svg'
         
     }
     ,
     {
         videoName   : 'Mortal Kombat Theme ' ,
-        videoPath   : '../videos/videos-list/video05.mp4' ,
+        videoPath   : 'videos/videos-list/video05.mp4' ,
         channelName : 'Trap Nation' ,
-        channelLogo : '../img/img-list/DTB.jpg'
+        channelLogo : 'img/img-list/DTB.jpg'
         
     }
 ]
